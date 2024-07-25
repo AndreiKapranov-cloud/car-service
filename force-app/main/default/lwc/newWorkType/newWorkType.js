e5d8e7c28ceabdaa951a5789ec777aa3e19ba095
Andrey Kapranov <akapranov872@gmail.com>
2024-07-25 19:01:36 +0300

    samodelkin

--- force-app/main/default/lwc/newWorkType/newWorkType.js
+++ force-app/main/default/lwc/newWorkType/newWorkType.js
@@ -1,6 +1,7 @@
 import { LightningElement,wire } from 'lwc';
 import getDurationTypePicklistValues from '@salesforce/apex/WorkTypeController.getDurationTypePicklistValues';
 import createWorkTypeApexMethod from '@salesforce/apex/WorkTypeController.createWorkTypeApexMethod';
+import getPicklistValuesUsingApex from '@salesforce/apex/WorkTypeController.getPicklistValuesUsingApex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 // import { createRecord } from 'lightning/uiRecordApi';
  import { getPicklistValues } from 'lightning/uiObjectInfoApi';
@@ -13,16 +14,17 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 const RECORD_TYPE_ID = '012000000000000AAA';
 
 export default class NewWorkType extends LightningElement {
-    
-  
+   
+    workTypeSObjectType = 'WorkType';
+    durationTypeFieldName = 'DurationType';
   
     workType;
     workTypeRecordList = [];
     
     workTypeName;
 
-
     picklistValues = [];
+
     name = '';
     description = '';
     estimatedDuration = '';
@@ -33,7 +35,6 @@ export default class NewWorkType extends LightningElement {
     showNewSkillRequirementComponent = false;
 
     workTypeRecordId;
-    timeoutId;
     
      
      handleChange(e) {
@@ -52,153 +53,100 @@ export default class NewWorkType extends LightningElement {
         }
       }
      
-      // @wire(getPicklistValues, {
-      //   recordTypeId: RECORD_TYPE_ID,
-      //   fieldApiName: DURATION_TYPE_FIELD,
-      // })
-      // getPicklistValuesForField({ data, error }) {
-      //   if (error) {
-      //     // TODO: Error handling
-      //     console.log('error');
-      //     console.error(error);
-      //   } else if (data) {
-      //     this.picklistValues = [...data.values];
+      // connectedCallback(){
+      //   getDurationTypePicklistValues()
+      //     .then(result => {
+      //       this.picklistValues = result;
+      //       console.log('this.picklistValues: ', this.picklistValues);
+      //     })
+      //     .catch(zalupa =>{
+      //       console.log(zalupa);
+      //       this.error = zalupa.message;
+      //       console.log('error createWorkType');
+      //       console.log(zalupa);
+      //       this.dispatchEvent(
+      //             new ShowToastEvent({
+      //                 title: 'Error getting PickList values',
+      //                 message: zalupa,
+      //                 variant: 'error'
+      //             })
+      //         );
+      //     });
       //   }
-      // }
-    
 
-
-     connectedCallback(){
-      getDurationTypePicklistValues()
-        .then(result => {
+      
+      connectedCallback(){
+          console.log('this.workTypeSObjectType,this.durationTypeFieldName =',this.workTypeSObjectType,this.durationTypeFieldName);
+          getPicklistValuesUsingApex({sObjectType:this.workTypeSObjectType,field:this.durationTypeFieldName})
+          .then(result => {
           this.picklistValues = result;
-          console.log('this.picklistValues: ', this.picklistValues);
-        })
-        .catch(zalupa =>{
+          console.log('this.picklistValues from apex: ', this.picklistValues);
+          })
+          .catch(error =>{
+            console.log(error);
+            this.massage = error.message;
+            console.log('Error getting PickList values');
             console.log(zalupa);
-              this.error = zalupa.message;
-              console.log('error createWorkType');
-              console.log(zalupa);
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error getting PickList values',
-                        message: zalupa,
-                        variant: 'error'
-                    })
-                );
+            this.dispatchEvent(
+                  new ShowToastEvent({
+                      title: 'Error getting PickList values',
+                      message: massage,
+                      variant: 'error'
+                  })
+              );
           });
-      }
+        }   
       
 
-        createWorkType() {
-
-          console.log('createWorkType');
-          createWorkTypeApexMethod(
-            {
-              workTypeName:this.name,
-              description:this.description,
-              estimatedDuration:this.estimatedDuration,
-              durationType:this.durationType
-            })
-          .then(result => {
-              console.log(result);
-              console.log('ID: ', result.Id);
-              this.workTypeObject = result;
-              this.workTypeRecordId = result.Id;
-              this.workTypeName = result.Name;
-             
-              console.log('workTypeObject = ' + this.workTypeObject);
-             
-              console.log('record.Name = ' + result.Name);
-            
-              this.showNewWorkTypeComponent = false;
-              this.showNewSkillRequirementComponent = true;
-
-
-              
-              const toastEvent = new ShowToastEvent({
-                  title:'Success!',
-                  message:'Work Type Record is created Successfully!',
-                  variant:'success'
-              });
-              this.dispatchEvent(toastEvent);
-            })
-            .catch(zalupa =>{
-              console.error(zalupa);
-                this.error = zalupa.message;
-                console.log('error createWorkType');
-                console.log(zalupa);
-                  this.dispatchEvent(
-                      new ShowToastEvent({
-                          title: 'Error creating Work Type record',
-                          message: zalupa,
-                          variant: 'error'
-                      })
-                  );
-            });
-      }
 
 
 
+      createWorkType() {
 
-
-
-
-
-
-
-       
-      //   const fields = {};
-       
-      //   fields[WORK_TYPE_NAME_FIELD.fieldApiName] = this.name;
-      //   fields[DESCRIPTION_FIELD.fieldApiName] = this.description;
-      //   fields[ESTIMATED_DURATION_FIELD.fieldApiName] = this.estimatedDuration;
-      //   fields[DURATION_TYPE_FIELD.fieldApiName] = this.durationType;
-      //   fields[SHOULD_AUTO_CREATE_SERVICE_APPOINTMENT_FIELD.fieldApiName] = this.shouldAutoCreateSvcAppt;
-
-        
-      //   const recordInput = { apiName: WORK_TYPE_OBJECT.objectApiName, fields:fields};
-      //   try {
-      //       const workType = createRecord(recordInput)
-            
-      //       .then(record => {
-      //         console.log(record);
-        
-      //        console.log('record.Name = ' + record.fields.Name.value);
-            
-      //      this.workTypeName = record.fields.Name.value;
-      //      this.workTypeObject = record;
-      //      console.log('workTypeObject = ' + this.workTypeObject);
-      //      console.log('workTypeName= ' + this.workTypeName);
-      // //  //       this.newWorkTypeRecord = record;
-            
-      //     //    console.log('record = ' + record);
-      //   //      console.log('newWorkTypeRecord = ' + this.newWorkTypeRecord);
-      //         this.workTypeRecordId = record.id;
-      //         this.showNewWorkTypeComponent = false;
-      //         this.showNewSkillRequirementComponent = true;
-      //       })
-            
-      //       this.dispatchEvent(
-      //           new ShowToastEvent({
-      //               title: 'Success',
-      //               message: 'Work Type created',
-      //               variant: 'success'
-      //           })
-      //       );
+      console.log('createWorkType');
+      console.log('estimated duration =' + this.estimatedDuration);
+      createWorkTypeApexMethod(
+        {
+          workTypeName:this.name,
+          description:this.description,
+          estimatedDuration:this.estimatedDuration,
+          durationType:this.durationType
+        })
+      .then(result => {
+          console.log(result);
+          console.log('ID: ', result.Id);
+          this.workTypeObject = result;
+          this.workTypeRecordId = result.Id;
+          this.workTypeName = result.Name;
           
-            
-      //   } catch (error) {
-      //     console.log('error createWorkType');
-      //     console.log(error);
-      //       this.dispatchEvent(
-      //           new ShowToastEvent({
-      //               title: 'Error creating Work Type record',
-      //               message: error,
-      //               variant: 'error'
-      //           })
-      //       );
-            
-      //   }
-    
+          console.log('workTypeObject = ' + this.workTypeObject);
+          
+          console.log('record.Name = ' + result.Name);
+        
+          this.showNewWorkTypeComponent = false;
+          this.showNewSkillRequirementComponent = true;
+
+
+          
+          const toastEvent = new ShowToastEvent({
+              title:'Success!',
+              message:'Work Type Record is created Successfully!',
+              variant:'success'
+          });
+          this.dispatchEvent(toastEvent);
+        })
+      .catch(zalupa =>{
+          console.error(zalupa);
+          this.error = zalupa.message;
+          console.log('error createWorkType');
+          console.log(zalupa);
+          this.dispatchEvent(
+              new ShowToastEvent({
+              title: 'Error creating Work Type record',
+              message: zalupa,
+              variant: 'error'
+                  })
+              );
+        });
+    }
 }
